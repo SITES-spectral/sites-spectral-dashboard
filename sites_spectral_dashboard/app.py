a99918deb528e5ebd49d435f0f30cc2e21060d5e
José Beltrán <102664984+jose-begeospatial@users.noreply.github.com>
2023-06-14 11:50:37 +0200

    SITES spectral

--- sites_spectral_dashboard/app.py
+++ sites_spectral_dashboard/app.py
@@ -1,6 +1,49 @@
+import os
 import streamlit as st
+from collections import OrderedDict
+from bgstools.io import get_available_services
+from bgstools.stt import build_activities_menu
 
-st.set_page_config(layout='wide')
+
+st.set_page_config(
+    layout='wide',
+    page_title='SITES spectral',
+    page_icon=':sun:',
+    initial_sidebar_state='expanded',
+    )
+
+
+def run():
+    LOGO_SITES_SPECTRAL = st.secrets['LOGOS']['LOGO_SIDEBAR_URL']
+    if LOGO_SITES_SPECTRAL: st.sidebar.image(
+            LOGO_SITES_SPECTRAL,             
+            caption= 'Swedish Infrastructure for Ecosystem Science (SITES) spectral'
+            )
+        
+    # Load the available services
+    SERVICES_FILEPATH = st.secrets['PATHS']['SERVICES_FILEPATH']
+    SERVICES_DIRPATH = st.secrets['PATHS']['SERVICES_DIRPATH']
+
+    # Load the yaml with core services as activities    
+    core_activities =  get_available_services(
+        services_filepath=os.path.abspath(SERVICES_FILEPATH)        
+    )
+       
+    build_activities_menu(
+            activities_dict=core_activities, 
+            label='**SITES spectral activities:**', 
+            key='activitiesMenu', 
+            services_dirpath=os.path.abspath(SERVICES_DIRPATH),
+            disabled=False
+            )
+
+
+if __name__ == '__main__':
+    run()
+else:
+    st.error('The app failed initialization. Report issue to mantainers in github')
+
+
 
 
 def main():
@@ -9,7 +52,7 @@ def main():
     if LOGO_SITES_SPECTRAL: st.sidebar.image(
             LOGO_SITES_SPECTRAL, 
             #width=150,
-            caption= 'Swedish Infrastructure for Ecosystem Science (SITES) spectral'
+            caption= '
             )
 
 if __name__ == '__main__':
